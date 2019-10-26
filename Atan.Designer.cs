e1ba53d17497511da76636d59418344d77451baa
Tailsdoll1994 <zexir710538@gmail.com>
2019-10-26 16:30:54 +0300

    Добавьте файлы проекта.

---
+++ Atan.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Atan
+{
+    partial class Atan
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Time_Button = new System.Windows.Forms.Button();
+            this.Button_LogicThread = new System.Windows.Forms.RadioButton();
+            this.Button_Logic = new System.Windows.Forms.RadioButton();
+            this.TheardWrite = new System.Windows.Forms.TextBox();
+            this.TheardMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(0, 224);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(501, 64);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(477, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Цикл вычислений";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(474, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(474, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите N число для произведения цикла";
+            // 
+            // Time_Button
+            // 
+            this.Time_Button.Location = new System.Drawing.Point(15, 81);
+            this.Time_Button.Name = "Time_Button";
+            this.Time_Button.Size = new System.Drawing.Size(473, 46);
+            this.Time_Button.TabIndex = 5;
+            this.Time_Button.Text = "Time";
+            this.Time_Button.UseVisualStyleBackColor = true;
+            this.Time_Button.Click += new System.EventHandler(this.Time_Button_Click);
+            // 
+            // Button_LogicThread
+            // 
+            this.Button_LogicThread.AutoSize = true;
+            this.Button_LogicThread.Checked = true;
+            this.Button_LogicThread.Location = new System.Drawing.Point(19, 134);
+            this.Button_LogicThread.Name = "Button_LogicThread";
+            this.Button_LogicThread.Size = new System.Drawing.Size(114, 17);
+            this.Button_LogicThread.TabIndex = 6;
+            this.Button_LogicThread.TabStop = true;
+            this.Button_LogicThread.Text = "Многопоточность";
+            this.Button_LogicThread.UseVisualStyleBackColor = true;
+            this.Button_LogicThread.CheckedChanged += new System.EventHandler(this.Button_LogicThread_CheckedChanged);
+            // 
+            // Button_Logic
+            // 
+            this.Button_Logic.AutoSize = true;
+            this.Button_Logic.Location = new System.Drawing.Point(19, 157);
+            this.Button_Logic.Name = "Button_Logic";
+            this.Button_Logic.Size = new System.Drawing.Size(108, 17);
+            this.Button_Logic.TabIndex = 7;
+            this.Button_Logic.Text = "Однопоточность";
+            this.Button_Logic.UseVisualStyleBackColor = true;
+            // 
+            // TheardWrite
+            // 
+            this.TheardWrite.Location = new System.Drawing.Point(140, 134);
+            this.TheardWrite.Name = "TheardWrite";
+            this.TheardWrite.Size = new System.Drawing.Size(46, 20);
+            this.TheardWrite.TabIndex = 8;
+            // 
+            // TheardMessage
+            // 
+            this.TheardMessage.AutoSize = true;
+            this.TheardMessage.Location = new System.Drawing.Point(192, 138);
+            this.TheardMessage.Name = "TheardMessage";
+            this.TheardMessage.Size = new System.Drawing.Size(271, 13);
+            this.TheardMessage.TabIndex = 9;
+            this.TheardMessage.Text = "Укажите количество ядер/логических процессеров";
+            // 
+            // Atan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 288);
+            this.Controls.Add(this.TheardMessage);
+            this.Controls.Add(this.TheardWrite);
+            this.Controls.Add(this.Button_Logic);
+            this.Controls.Add(this.Button_LogicThread);
+            this.Controls.Add(this.Time_Button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Atan";
+            this.Text = "Многопоточность";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Time_Button;
+        private System.Windows.Forms.RadioButton Button_LogicThread;
+        private System.Windows.Forms.RadioButton Button_Logic;
+        private System.Windows.Forms.TextBox TheardWrite;
+        private System.Windows.Forms.Label TheardMessage;
+    }
+}
+
---
+++ Atan.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Atan
+{
+    public partial class Atan : Form
+    {
+        public Atan()
+        {
+            InitializeComponent();
+        }
+        private void Time_Button_Click(object sender, EventArgs e)
+        {
+            if (Button_LogicThread.Checked)
+            {
+                textBox1.Text = TimeThread(Convert.ToUInt64(textBox2.Text));
+            }
+            else if (Button_Logic.Checked)
+            {
+                textBox1.Text = Time(Convert.ToUInt64(textBox2.Text));
+            }
+            else
+            {
+                MessageBox.Show("Взаимоисключающая ошибка");
+            }
+        }
+        private void Formula(UInt64 num)
+        {
+            Random rndx = new Random();
+            Random rndy = new Random();
+
+            double radians;
+            double angle;
+
+            for (UInt32 i = 0; i < num; i++)
+            {
+                radians = Math.Atan2(rndx.NextDouble(), rndy.NextDouble());
+                angle = radians * (180 / Math.PI);
+            }
+        }
+        private string TimeThread(UInt64 num)
+        {
+            List<Thread> t = new List<Thread>();
+            var start = DateTime.Now;
+            for (int j = 0; j < Convert.ToInt32(TheardWrite.Text); j++)
+            {
+                t.Add(new Thread(() =>
+                {
+                    Formula(num / Convert.ToUInt64(TheardWrite.Text));
+                }));
+                t[j].Start();
+            }
+            for (int j = 0; j < Convert.ToInt32(TheardWrite.Text); j++)
+            {
+                t[j].Join();
+            }
+            return (DateTime.Now - start).ToString();
+        }
+        private string Time(UInt64 num)
+        {
+            Random rndx = new Random();
+            Random rndy = new Random();
+
+            double radians;
+            double angle;
+            var start = DateTime.Now;
+            for (UInt64 i = 0; i < num; i++)
+            {
+                radians = Math.Atan2(rndx.NextDouble(), rndy.NextDouble());
+                angle = radians * (180 / Math.PI);
+            }
+            var timeEnd = DateTime.Now - start;
+
+            return timeEnd.ToString();
+        }
+
+        private void Button_LogicThread_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Button_LogicThread.Checked.Equals(true))
+            {
+                TheardWrite.Enabled = true;
+                TheardMessage.Enabled = true;
+            }
+            else
+            {
+                TheardWrite.Enabled = false;
+                TheardMessage.Enabled = false;
+            }
+        }
+    }
+}
